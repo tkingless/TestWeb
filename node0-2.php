1f9169ccf7af9871c762d31f9816c920fb11b89b
kingkwun.tsang <kingkwun.tsang@LS-GAMP102291.(none)>
2014-04-29 10:40:12 +0800

    company third commit

---
+++ node0-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Child Page (php)</h1>
+
+<p>node 0-2</p>
+
+<? phpinfo(); ?>
+
+<?php
+echo "Hello World!";
+?>
+
+<a href='index.html'>Back to index page</a>
+
+</body>
+</html>
