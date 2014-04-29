0d43799b44dca60c41c1e297f6d4469f00010a03
kingkwun.tsang <kingkwun.tsang@LS-GAMP102291.(none)>
2014-04-29 18:53:32 +0800

    php basic input react

--- node0-2.php
+++ node0-2.php
@@ -9,8 +9,28 @@
 <? phpinfo(); ?>
 
 <?php
-echo "Hello World!";
+
+$con = mysql_connect("localhost","root","a97077");
+
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  else
+  {
+  	echo "connect success!<br /><br />";
+  	mysql_select_db("cdcol");
+  }
+
+echo "Hello World!<br /><br />";
+
 ?>
+
+<form action="node0-2-0.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
 
 <a href='index.html'>Back to index page</a>
 
